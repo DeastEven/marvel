78cebcc37febf7250ec4c9fa5466a47b4d41ff1b
ArturNovikov <74718292+DeastEven@users.noreply.github.com>
2021-05-17 20:58:25 +0300

    Add files via upload

---
+++ src/components/Error/Error.js
@@ -0,0 +1,21 @@
+import {ROOT_INDEX} from '../../constants/root';
+
+import './Error.css';
+
+
+class Error{
+
+    render(){
+        const htmlWrapper=`
+            <div class="error__container">
+                <span>
+                    <p class="error__alert">Произошла ошиба</p>
+                    <p class="error__alert">Попробуйте зайти позже</p>
+                </span>
+            </div>
+        `;
+        ROOT_INDEX.innerHTML=htmlWrapper;
+    }
+}
+
+export default new Error();
